7fd279938c94ea1c6030c51b68703e7664a45bac
M. Alif Raher <alifraher@gmail.com>
2018-08-10 10:15:11 +0700

    Change data and table name

--- seeders/20180809042347-demo-employees.js
+++ seeders/20180809042347-demo-employees.js
@@ -4,48 +4,48 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
     const demoEmployees = [{
-      birthdate: '10/10/1990',
+      birth_date: '10-10-1990',
       first_name: 'Berto',
-      last_name: 'Kohling',
+      last_name: 'Alex',
       gender: 'M',
-      hire_date: '10/10/2010',
+      hire_date: '10-10-2010',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      birthdate: '10/10/1991',
-      first_name: 'Bert',
-      last_name: 'Kohling',
+      birth_date: '10-10-1991',
+      first_name: 'Karl',
+      last_name: 'Steven',
       gender: 'F',
-      hire_date: '10/10/2011',
+      hire_date: '10-10-2011',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      birthdate: '10/10/1992',
-      first_name: 'Ber',
-      last_name: 'Kohling',
+      birth_date: '10-10-1992',
+      first_name: 'John',
+      last_name: 'Cena',
       gender: 'M',
-      hire_date: '10/10/2012',
+      hire_date: '10-10-2012',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      birthdate: '10/10/1993',
-      first_name: 'Berto',
+      birth_date: '10-10-1993',
+      first_name: 'Roberto',
       last_name: 'Kohlin',
       gender: 'M',
-      hire_date: '10/10/2013',
+      hire_date: '10-10-2013',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      birthdate: '10/10/1994',
-      first_name: 'Bert',
-      last_name: 'Kohl',
+      birth_date: '10-10-1994',
+      first_name: 'James',
+      last_name: 'Bond',
       gender: 'F',
-      hire_date: '10/10/2014',
+      hire_date: '10-10-2014',
       createdAt: new Date(),
       updatedAt: new Date()
     }]
 
-    return queryInterface.bulkInsert('Employees', demoEmployees, {});
+    return queryInterface.bulkInsert('employees', demoEmployees, {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -56,6 +56,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('Person', null, {});
     */
-    return queryInterface.bulkDelete('Employees', null, {});
+    return queryInterface.bulkDelete('employees', null, {});
   }
 };
